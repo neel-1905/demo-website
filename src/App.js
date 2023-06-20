56897f662e663a99b58f26fd94d6d38032462066
neel-1905 <neelnarwadkar@gmail.com>
2023-06-20 10:17:01 +0530

    changes in app new

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            {/* dasasadsasad */}
+            
           </Routes>
         </Router>
       </div>
